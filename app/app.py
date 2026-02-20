63c88a7085275bd7b5996f6f331ab4bdff3ab3f1
amitb1989 <amit.bapodara@hotmail.com>
2026-02-20 10:02:35 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Deployed via CodeDeploy/CodeBuild pipeline!", 200
+
+@app.route("/healthz")
+def healthz():
+    return "ok", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
