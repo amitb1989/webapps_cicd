d9f16102ea27409073bdf6c798ca2652b3b86412
amitb1989 <amit.bapodara@hotmail.com>
2026-03-03 10:09:04 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-    return "Deployed via CodePipeline CI/CD done!!!!!!!!", 200
+    return "Deployed via CodeDeploy/CodeBuild pipeline!", 200
 
 @app.route("/healthz")
 def healthz():
