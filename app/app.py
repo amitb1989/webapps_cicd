10a1a96b703f1d9daf93cfcd04ba2d85f5e69028
Dilipbhai <bapodara.amit-d@capgemini.com>
2026-03-05 16:01:28 +0530

    deploy web app

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Deployed via CodeDeploy/CodeBuild pipeline for webapps", 200
+    return "Deployed via CodeDeploy/CodeBuild pipeline !!!!!!", 200
 
 @app.route("/healthz")
 def healthz():
